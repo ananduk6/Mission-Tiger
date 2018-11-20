f3956e48c492630546d689e567fb5f39b8528cfe
Anandu <ananduk6@gmail.com>
2018-11-20 15:29:00 +0530

    Add files via upload

---
+++ donation.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Mission Tiger</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/tron-ui.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/reset.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+   
+    
+ 
+   <!-- home section
+================================================== -->
+  
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top nav-test">
+          <div class="container">
+          
+            <div class="navbar-header">
+            <a class="navbar-brand" href="index.html"><img src="assets/images/logo.jpg" alt="mission tiger film" width="175px"></a>
+            
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              
+            </div>
+            <div id="navbar" class="navbar-collapse collapse p30">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.html">Home</a></li>
+                <li class="active"><a href="about.html">About Us</a></li>
+                <li ><a href="mission-film.html">Mission Tiger - The Film</a></li>
+                <li><a href="articles.html">Articles and Campaign</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="assets/images/banner/i-banner.jpg" alt="First slide">
+         
+        </div>
+      </div>
+     
+    </div><!-- /.carousel -->
+   
+   
+   
+   
+   
+  <!-- about section
+================================================== -->
+<section id="about" class="paralla-section">
+
+	<div class="container">
+        
+        
+			
+			<div class="col-sm-6 col-xs-12">
+				<div class="about-des">
+					<h2>Donation</h2>
+					
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                   
+				</div>
+			</div>
+
+      <div class="col-sm-6 col-xs-12">
+        <div class="about-des">
+          <form method="post" action="#">
+          <div class="col-md-12 col-sm-12">
+            <input type="text" id="name" name="name" placeholder="Name" class="form-control">
+            <input type="email" id="email" name="email" placeholder="Email" class="form-control">
+            <input type="text" name="phone" placeholder="Phone" class="form-control">
+            <textarea message="message" placeholder="Message" id="message" class="form-control" rows="8" name="message"></textarea>
+          </div>
+          <div class="col-md-6 col-sm-12">
+            <input type="submit" value="Send" id="submit" class="more_info" name="submit">
+          </div>
+          <div class="clearfix"></div>
+        </form>
+                   
+        </div>
+      </div>
+
+            </div>
+           
+	
+</section>
+
+
+
+
+
+<!-- our News section
+================================================== -->
+<section id="film" class="paralla-section film-back">
+	<div class="container">
+		<div class="row">
+        <div class="text-center">
+        <h2>Our Team</h2>
+        </div>
+        
+        <div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="Hari" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>Hari</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="James Lewis" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>James Lewis</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+<div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="James Lewis" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>James Lewis</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+<div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="James Lewis" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>James Lewis</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+<div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="James Lewis" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>James Lewis</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+<div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="James Lewis" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>James Lewis</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+<div class="col-sm-4 col-md-3">
+          <div class="team-member">
+            <img alt="James Lewis" src="assets/images/teams/lewis.jpg" class="thumb img-circle">
+            <h4>James Lewis</h4>
+            <span class="text-muted">Financial Director</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div class="social-buttons">
+              <a class="social-btn sb-linkedin sb-sm" href="#"><i class="fa fa-linkedin"></i></a>
+              <a class="social-btn sb-email sb-sm" href="#"><i class="fa fa-envelope"></i></a>
+              <a class="social-btn sb-facebook sb-sm" href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+          </div>
+        </div>
+
+        
+        
+        
+        
+       
+            
+   
+		</div>
+	</div>
+</section>
+
+<!-- Footer section
+================================================== -->
+<footer class="footer-wrp">
+	<div class="container">
+		
+        <div class="row">
+			<div class="col-xs-12 wow fadeInUp pt15-sm" data-wow-delay="0.3s">
+				<p class="copyright FL">Copyright &copy; 2016 <span>Mission Tiger Team. All rights reserved.</span></p>
+           <p class="copyright FR">Design & Developed: <a href="#" target="_blank"><b>Fortune Cinimas</b></a></p>
+			</div>
+
+		</div>
+	</div>
+
+</footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script>
+	  new WOW({ mobile: false }).init();
+    </script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+  </body>
+</html>
